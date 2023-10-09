d23c279d000a7d9d85cbf2d5ac23dd665b7cbbac
ppdfr84 <pedro.rocha@csjsistemas.com.br>
2023-10-08 21:39:47 -0300

    Add botão home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import './Heder.css'
+import { AiFillHome } from "react-icons/ai";
+
 
 const Header = () => {
     return(
         <div className="header-container">
+            <div className="home-button-content" >
+                <a href="/" rel="noopener noreferrer"><AiFillHome className="home-btn" /></a>
+            </div>
         </div>
     )
 }
